45b5a957186d7481156ae83f3f0af81c83d497c1
Samila Castro <samilav1997@gmail.com>
2021-07-26 10:55:43 -0300

    Cria view da pag de cadastro de entidade

--- src/main.js
+++ src/main.js
@@ -8,9 +8,13 @@ import './global.css';
 
 
 
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+
 
 
 import router from "./router";
 const app = createApp(App);
+app.use(ElementPlus)
 app.use(router);
 app.mount("#app");
