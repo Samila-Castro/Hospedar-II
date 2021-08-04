cf427809a237753b20255b9efc77f5afee565fed
franciscoantoniosousa@alu.ufc.br <franciscoantoniosousa@alu.ufc.br>
2021-08-04 16:05:29 -0300

    add script

--- src/views/ClientLogin/index.vue
+++ src/views/ClientLogin/index.vue
@@ -22,3 +22,44 @@
         </div>
     </div>
 </template>
+
+<script>
+import api from "../../services/api"
+import Button from '../../components/Button.vue'
+import Description from '../../components/Description.vue'
+
+export default {
+  name: 'ClientLogin',
+        components: {
+            Button,
+            Description
+        },
+        methods: {
+            async submitForm(e) {
+                try {
+                    e.preventDefault()
+                    const result = await api.post('/session', this.form)
+                    console.log(result.data);
+                    localStorage.setItem('@hospedar-login', JSON.stringify(result.data));
+                    alert("Login com sucesso!")
+                     this.$router.push(`/entidade-list`);
+                } catch (error) {
+                    console.log(error);
+                    alert("Erro ao tentar login!")
+                }
+            },
+
+        },
+
+        data() {
+            return {
+                form: {
+                    email: '',
+                    password: ''
+                }
+            }
+        },
+
+
+}
+</script>
